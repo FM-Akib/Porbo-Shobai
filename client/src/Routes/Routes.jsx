027f2243ba3ef35183692a4e8d6a5419e199846f
FM-Akib <muntasirakib08@gmail.com>
2024-11-26 02:48:45 +0600

    (chore-Error404):Error 404 page are added

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 
 import MainLayout from "@/Layouts/MainLayout";
 import Competitions from "@/Pages/Competitions";
+import ErrorPage from "@/Pages/Error404";
 import Landing from "@/Pages/Landing";
 import Register from "@/Pages/Register/Register";
 import {createBrowserRouter} from "react-router-dom";
@@ -10,6 +11,7 @@ export const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout/>,
+      errorElement: <ErrorPage/>,
       children: [
         {
           path: "/",
