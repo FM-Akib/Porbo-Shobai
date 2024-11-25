cdc6ab9fcb1c1d5ca883e93ca38f0b679f9c88d4
FM-Akib <muntasirakib08@gmail.com>
2024-11-25 11:33:37 +0600

    (chore-userRoute):setup user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getUsers } = require("../controllers/userController");
+const { getUsers, postAuser, updateAuser, deleteAuser, getAuser
+ } = require("../controllers/userController");
 
 const router = express.Router();
 
 // Define routes for the user module
 router.get("/", getUsers);
+router.get("/:email", getAuser);
+router.post("/", postAuser);
+router.patch("/:id", updateAuser);
+router.delete("/:id", deleteAuser);
 
 module.exports = router;
