806ba29ead3db07c94ffc68d7466afdb1ca5e364
FM-Akib <muntasirakib08@gmail.com>
2024-11-29 11:09:43 +0600

    feat(Host-Form-error): Zod error handled

--- client/src/utils/FormError.js
+++ client/src/utils/FormError.js
@@ -1,13 +1,14 @@
-import * as z from "zod"
+import { z } from "zod"
 
 // Define the schema within the same file
 export const formSchema = z.object({
-    title: z.string().min(1, "Title is required").max(190, "Title must be 190 characters or less"),
+    title: z.string().min(1, { message: "Title is required"}).max(190, { message: "Title must be 190 characters or less"}),
     organization: z.string().min(1, "Organization is required").max(190, "Organization must be 190 characters or less"),
     websiteUrl: z.string().url("Invalid URL"),
     opportunityType: z.string().min(1, "Opportunity type is required"),
     mode: z.string().min(1, "Mode is required"),
     visibility: z.string().min(1, "Visibility is required"),
-    content: z.string().min(1, "Description is required"),
-    // ... other fields
+    subtitle: z.string().min(1, "Subtitle is required").max(190, "Subtitle must be 190 characters or less"),
+    city: z.string().min(1, "City is required"),
+    location: z.string().min(1, "Location is required"),
   })
