b08a0a3fa6ca57c68d277c50a3ec3cb7f00a5685
Rafidul01 <rafidulhasan555@gmail.com>
2024-11-30 12:58:49 +0600

    Register user complite

--- client/src/hooks/useAxiosSecure.jsx
+++ client/src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: import.meta.env.VITE_server_url,
     
 })
 const useAxiosSecure = () => {
