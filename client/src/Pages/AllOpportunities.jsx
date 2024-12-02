3f6b82411c5cc302534ae380875032bfd5958334
FM-Akib <muntasirakib08@gmail.com>
2024-12-02 13:29:39 +0600

    Chore(Opportunity-card): Opportunity card added

--- client/src/Pages/AllOpportunities.jsx
+++ client/src/Pages/AllOpportunities.jsx
@@ -1,10 +1,25 @@
 import OpportunityCard from "@/components/AllOpportunities/OpportunityCard";
+import Loader from "@/components/shared/Loader";
+import useOpportunities from "@/Hooks/useOpportunities";
 
 const AllOpportunities = () => {
+    const [opportunities] = useOpportunities();
+    console.log(opportunities);
     return (
-        <>
-         <OpportunityCard/>   
-        </>
+        <section className="max-w-7xl mx-auto">
+        {
+            opportunities.length === 0 && (<Loader/>)
+        }
+        {
+            opportunities?.length > 0 && (
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {opportunities.map(opportunity=>(
+                        <OpportunityCard key={opportunity._id} opportunity={opportunity}/>
+                    ))}
+                </div>
+            )
+        }
+        </section>
     );
 };
 
