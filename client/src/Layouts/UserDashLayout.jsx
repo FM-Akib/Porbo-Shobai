19f0a286e86dab04c4df0c760ce5cffd29e0a350
FM-Akib <muntasirakib08@gmail.com>
2024-12-17 11:23:42 +0600

    user dashboard updated

--- client/src/Layouts/UserDashLayout.jsx
+++ client/src/Layouts/UserDashLayout.jsx
@@ -8,7 +8,7 @@ const UserDashLayout = () => {
       <Sidebar />
       </div>
       <div className="mt-12 md:mt-0 col-span-10 flex flex-col overflow-hidden">
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-yellow-200 dark:bg-gray-800">
           <Outlet />
         </main>
       </div>
