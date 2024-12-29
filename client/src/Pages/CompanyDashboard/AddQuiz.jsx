13f7551302effc5156e4cdf412ff619da591f57a
FM-Akib <muntasirakib08@gmail.com>
2024-12-29 22:06:44 +0600

    Redux added post a opportunity bug resolved

--- client/src/Pages/CompanyDashboard/AddQuiz.jsx
+++ client/src/Pages/CompanyDashboard/AddQuiz.jsx
@@ -1,19 +1,31 @@
 import BrowseParticipants from "@/components/Quiz/BrowseParticipants";
 import QuizBrowser from "@/components/Quiz/QuizBrowser";
 import QuizCreator from "@/components/Quiz/QuizCreator";
+import Loader from "@/components/shared/Loader";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useGetOpportunitiesByIdsQuery } from "@/redux/api/api";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const AddQuiz = () => {
-  const location = useLocation();
-  const { opportunity } = location.state || {}; 
-  const { title } = opportunity;
+  const { id } = useParams();
   const [activeTab, setActiveTab] = useState("browse"); // Default tab
+
+  const {
+      data,
+      isLoading,
+      isError,
+  } = useGetOpportunitiesByIdsQuery([id]);
+  if (isLoading) return <Loader />;
+  if (isError) return <div>Error</div>
+  const opportunity = data[0];
+  const { title } = opportunity;
+
 
   const handleEditTask = () => {// Set the task to be edited
     setActiveTab("create"); // Switch to the "Create Quiz" tab
   };
+ 
     return (
         <div className="min-h-screen ">
         <div className="container mx-auto p-6">
@@ -36,11 +48,15 @@ const AddQuiz = () => {
           </TabsContent>
           
           <TabsContent value="browse">
-            <BrowseParticipants opportunity={opportunity} />
+            <BrowseParticipants 
+            opportunity={opportunity}
+            />
           </TabsContent>
           
           <TabsContent value="create">
-            <QuizCreator opportunity={opportunity}  />
+            <QuizCreator 
+            opportunity={opportunity} 
+             />
           </TabsContent>
         </Tabs>
       </div>
