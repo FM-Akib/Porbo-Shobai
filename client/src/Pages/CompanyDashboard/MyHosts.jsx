44ab0a318bf69e3fcff9acf3db7b9de5ad37ce40
FM-Akib <muntasirakib08@gmail.com>
2024-12-30 00:35:21 +0600

    Redux deleted

--- client/src/Pages/CompanyDashboard/MyHosts.jsx
+++ client/src/Pages/CompanyDashboard/MyHosts.jsx
@@ -1,8 +1,10 @@
 import OpportunitiesTable from "@/components/DashboardCompany/OpportunitiesTable";
 import TitleDashboard from "@/components/DashboardUser/TitleDashboard";
 import Loader from "@/components/shared/Loader";
+import { ToastAction } from "@/components/ui/toast";
+import { toast } from "@/Hooks/use-toast";
 import useUserInfo from "@/Hooks/useUserInfo";
-import { useGetOpportunitiesByIdsQuery } from "@/redux/api/api";
+import { useDeleteOpportunityMutation, useGetOpportunitiesByIdsQuery } from "@/redux/api/api";
 
 const MyHosts = () => {
     const { userInfo, isLoading: loadUserInfo } = useUserInfo();
@@ -13,17 +15,31 @@ const MyHosts = () => {
     } = useGetOpportunitiesByIdsQuery(userInfo?.hosts?.join(','), {
         skip: !userInfo?.hosts || userInfo?.hosts.length === 0, // Skip if no hosts
     });
+    const [deleteOpportunity, { isLoading:deleteLoading }] = useDeleteOpportunityMutation();
 
-    const handleDelete =  (id) => {
-        console.log("Delete opportunity with ID:", id);
-        // try {
-        //     await axiosSecure.delete(`/opportunities/${id}`);
-        //     setMyhosts(prev => prev.filter(host => host._id !== id));
-        // } catch (error) {
-        //     console.error("Error deleting opportunity:", error.message);
-        // }
+    const handleDelete = async (id) => {
+        try {
+            // Make the API call to delete the opportunity
+            await deleteOpportunity(id).unwrap(); 
+            // Show success toast notification
+            toast({
+              variant: "default",
+              title: "Opportunity!",
+              description: "Opportunity deleted successfully.",
+              action: <ToastAction altText="ok">OK!</ToastAction>,
+              className: "bg-green-500 text-white",
+            })
+          } catch {
+            toast({
+              variant: "destructive",
+              title: "Error!",
+              description: "Failed to delete opportunity.",
+              action: <ToastAction altText="Try again">Try again</ToastAction>,
+              className: "bg-red-500 text-white",
+            })      
+          }
     };
-    
+    if (deleteLoading) return <Loader />;
     if (isLoading || loadUserInfo) return <Loader />;
     if (isError) return <div className="text-center text-red-500 font-bold text-xl mt-8">
         Something went wrong. Please try again later.</div>;
