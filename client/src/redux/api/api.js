3d4c0354b5e3bb5a0110e0da32a90051c3143621
FM-Akib <muntasirakib08@gmail.com>
2024-12-29 20:25:44 +0600

    Redux bug solved and fetch for single opportunity

--- client/src/redux/api/api.js
+++ client/src/redux/api/api.js
@@ -3,19 +3,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://porbo-shobai-sever.vercel.app' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_server_url }),
   //this is a name of cache todo, Ekhane aro tag type add hote pare.
   tagTypes: ['Opportunities'],
   endpoints: (builder) => ({
+    
     getOpportunities: builder.query({
-     
         query: ({ filters = {}, page = 1, limit = 10 })=>{
             const queryParams = new URLSearchParams({
                 ...filters,
                 page,
                 limit,
               }).toString();
-         
               return {
                 url: `/opportunities?${queryParams}`,
                 method: 'GET',
